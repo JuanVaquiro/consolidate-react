166aa70563259a74958a28910578d58e05cad002
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-03-20 11:30:51 -0500

    order

---
+++ projects/05-buscador-pruebaTC-useCallback/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App () {
+  return (
+    <div className='page'>
+      <header>
+        <h1>Buscaodr de peliculas</h1>
+        <form className='form'>
+          <input type='text' placeholder='pelicula' />
+          <button type='submit'> Buscar </button>
+        </form>
+      </header>
+      <main>
+        pelicuas
+      </main>
+    </div>
+  )
+}
+
+export default App
