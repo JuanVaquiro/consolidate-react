75a36d1aa78d046ed224b8125d3b4c1153313353
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-03-31 10:05:27 -0500

    order class

---
+++ JoyReact/project-the-joy=react/src/App.jsx
@@ -0,0 +1,12 @@
+import StickerPad from './class/Sticker/StickerPad'
+
+function App () {
+  return (
+    <div className='App'>
+      <p>Click around to add stickers!</p>
+      <StickerPad />
+    </div>
+  )
+}
+
+export default App
